89cadcbb4b7297b5f435e13ec770979a1b797f74
L30Zh <LEoz47@nycstudents.net>
2025-09-15 10:13:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,47 @@
+""" x = 3
+y = float(3)
+print(x, y) """
 
-""" x=3
-y=float(3)
-print(x,y)
 
+
+""" 
 values = [1,2.23,5,7,2,30,15]
-print(values[0])
-print(values[6])
-for i in values:
-    print(1) """
+print(values)
+print (values[0])
+print (values[6])
+
+ """
+
+# == is equal to an evaluation
+""" 
+x = 15
+
+if x == 16:
+    print("x is 15")
+elif x > 15:
+    print("x is greater than 15")
+else:
+    print("x is less than 15") """
 
 
-x="This is a thing"
-y=x.split()
-z=y[0]
-print(y)
-print(z)
+""" values = [1,2.23,5,8,2,30,15]
+if values[3] == 7:
+    print("The fourth value is 7")
+elif values[3] > 7:
+    print("The fourth value is greater than 7")
+else:
+    print("The fourth value is less than 7") """
+
+
+""" DayWeek = input("What day is it?")
+
+if DayWeek == "Monday" "it is Monday":
+    print("Correct")
+else: print("Incorrect")
+
+ """
+
+
+x = "2 + 2"
+Question = input(f"What is, {x}?")
+
